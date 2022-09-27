420313bde1d57d685aca5f08363c6afe8fd791a8
hoseong511 <thdghtjd115@naver.com>
2022-09-27 22:28:24 +0900

    Update main : 01.1.3 1_32 recursive ver 1, 2

--- 01/1.3/1_32.scm
+++ 01/1.3/1_32.scm
@@ -5,6 +5,19 @@
 			(iter (next a) (combiner result (term a)))))
 	(iter a null-value))
 
+(define (accumulate-re1 combiner null-value term a next b)
+	(if (> a b)
+		null-value
+		(combiner (term a)
+				  (accumulate-re1 combiner null-value term (next a) next b))))
+				  
+(define (accumulate-re2 combiner null-value term a next b)
+	(define (iter a)
+		(if (> a b)
+			null-value
+			(combiner (iter (next a)) (term a))))
+	(iter a))
+
 (define (sum term a next b)
 	(accumulate + 0 term a next b))
 
