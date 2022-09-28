f8bf32b924733a95a0b46630fd4c6b39c88795b6
hoseong511 <thdghtjd115@naver.com>
2022-09-28 23:08:05 +0900

    Update main : 01.1.3 1_45 종합

---
+++ 01/1.3/1_45.scm
@@ -0,0 +1,48 @@
+(define tolerance 0.00001)
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (square x)
+  (* x x))
+
+(define (fixed-point f first-guess)
+	(define (close-enough? x y)
+		(< (abs (- x y)) tolerance))
+	(define (try guess)
+		(let ((next (f guess)))
+			(if (close-enough? guess next)
+				next
+				(try next))))
+	(try first-guess))
+
+(define (compose f g)
+	(lambda (x) (f (g x))))
+
+(define (repeated f n)
+	(if (= n 1)
+		(lambda (x) (f x))
+		(repeated (compose f f) (- n 1))))
+
+(define (n-th f n)
+	(if (= n 1)
+		(lambda (x) (f x))
+		(n-th (lambda (x) (* x (f x)) (- n 1)))))
+
+(define (avg-damp f)
+	(lambda (x) (average x (f x))))
+
+(define (n-th-sqrt x n)
+	(define (n-avg-damp n)
+		(repeated avg-damp n))
+	(define (n-th-x n)
+		(n-th (lambda (x) x) ))
+	(if (< n 2)
+		(error "n > 1!")
+		(let ((n-temp 
+							((n-avg-damp (- n 1)) (lambda (y) (/ x ((n-square (- n 2)) y))))))
+		(fixed-point n-temp 1.0)))
+		)
+	
+(n-th-sqrt 4 2)
+((n-th (lambda (x) x) 2) 2)
