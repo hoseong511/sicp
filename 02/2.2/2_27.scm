4178b50230428e6a3cb204ca1b1af8aa3a6be1bd
hoseong511 <thdghtjd115@naver.com>
2022-10-04 20:32:04 +0900

    Update main : 02.2.2 연습문제 2_27 수정

--- 02/2.2/2_27.scm
+++ 02/2.2/2_27.scm
@@ -6,10 +6,13 @@
   (iter x nil))
 
 (define (deep-reverse x)
-  (cond ((null? x) nil)
-        ((not (list? (car x))) (reverse x))
-        (else (cons (deep-reverse (car x))
-                      (deep-reverse (cdr x))))))
+  (define (iter things res)
+    (if (null? things)
+        res
+        (if (pair? (car things))
+            (iter (cdr things) (cons (deep-reverse (car things)) res))
+            (iter (cdr things) (cons (car things) res)))))
+  (iter x nil))
 
 (define x1 (list (list 1 2) (list 3 4) (list 5 6)))
 (reverse x1)
