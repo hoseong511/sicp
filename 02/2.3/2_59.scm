506b2bcf2f1d434f47a25843ef0aab3d4062bfed
hoseong511 <thdghtjd115@naver.com>
2022-10-25 22:43:45 +0900

    Update main : 02.2.3 연습문제 2_60, 중복제거하기

--- 02/2.3/2_59.scm
+++ 02/2.3/2_59.scm
@@ -22,7 +22,24 @@
         ((element-of-set? (car set1) set2)
          (union-set (cdr set1) set2))
         (else (cons (car set1) (union-set (cdr set1) set2)))))
-       
-(define set1 (list))
-(define set2 (list 5 6 1 4))
-(union-set set1 set2)
+
+(define (unique set)
+  (cond ((null? set) '())
+        ((element-of-set? (car set) (cdr set))
+         (unique (cdr set)))
+        (else (cons (car set) (unique (cdr set))))))
+
+(define (union-set2 set1 set2)
+  (define (union-set set1 set2)
+    (cond ((null? set1) set2)
+          ((element-of-set? (car set1) set2)
+           (union-set (cdr set1) set2))
+          (else (cons (car set1) (union-set (cdr set1) set2)))))
+  (let ((uni-set1 (unique set1))
+        (uni-set2 (unique set2)))
+    (union-set uni-set1 uni-set2)))
+
+(define set1 (list 1 1 2 2 3 3 4 4))
+(define set2 (list 5 6 1 4 5 5 5))
+(union-set2 set1 set2)
+(unique (list 2 3 2 1 3 2 2))
